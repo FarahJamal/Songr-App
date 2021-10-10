53411c8f0b3e4f41ce1e9754f825d03499e12356
FarahJamal <farahsarese@gmail.com>
2021-10-10 14:04:00 +0300

    initial commit from Spring init

---
+++ src/main/java/me/farah/songrapp/SongrAppApplication.java
@@ -0,0 +1,13 @@
+package me.farah.songrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongrAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/farah/songrapp/SongrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.farah.songrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
